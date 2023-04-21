6874cb187e9317a9f84efb4372bd070f6d93fa7c
Lapololucy <lapololucy@gmail.com>
2023-04-21 05:01:43 -0700

    creating a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in the /tem directory
+ile { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
