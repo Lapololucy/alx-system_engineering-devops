093d92149ce669709d9f3e2d1eafbba4c4543764
Lapololucy <lapololucy@gmail.com>
2023-04-21 05:04:04 -0700

    Task 0, creating a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
-# create a file in the /tem directory
-ile { '/tmp/school':
-  ensure  => file,
+# A puppet manifest that creates a file school in /tmp.
+# The file (School) has permissions 0744, belonging to the user and group
+# www-data.
+file { '/tmp/school':
+  ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
