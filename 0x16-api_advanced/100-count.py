0f50ca45d3ab853cebcf530b33ad6d3d3722f945
Lapololucy <lapololucy@gmail.com>
2023-06-07 06:54:51 +0000

    advanced task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,65 @@
 #!/usr/bin/python3
-"""Module for task 3"""
+"""
+Module for requesting the Reddit API
+"""
+import requests
+URL = 'https://www.reddit.com'
+headers = {"User-Agent": "Custom Agent"}
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
+def count_words(subreddit, word_list):
+    """Prints a count of given keywords"""
+    try:
+        uri = URL + '/r/{}/hot.json'.format(subreddit)
+        hot_titles = fetch_hot_posts(uri)
+        pure_word_list = sanitize(word_list)
+        result = dict(zip(pure_word_list, [0] * len(pure_word_list)))
+        for title in hot_titles:
+            for word in pure_word_list:
+                occurrences = count_occurrences(word, title.split())
+                if occurrences:
+                    result[word] += occurrences
+        result = dict(sorted(result.items()))
+        for key, value in result.items():
+            if value:
+                print("{}: {}".format(key.lower(), value))
+        return result
+    except Exception as e:
+        print(e)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
 
-    info = sub_info.json()
+def fetch_hot_posts(uri, after=None, hot_list=[]):
+    """Recursive function for the job"""
+    parameters = {}
+    if after:
+        parameters['after'] = after
+    res = requests.get(uri,
+                       headers=headers,
+                       allow_redirects=False,
+                       params=parameters)
+    if res.status_code == 200:
+        children = res.json().get('data').get('children')
+        for child in children:
+            hot_list.append(child.get('data').get('title'))
+        next_ = res.json().get('data').get('after')
+        if next_ is not None:
+            fetch_hot_posts(uri, next_, hot_list)
+        return hot_list
 
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
 
-    word_list = list(dict.fromkeys(word_list))
+def count_occurrences(word, corpus):
+    """Counts occurrences of a word in a list of words"""
+    word = word.lower()
+    count = 0
+    for w in corpus:
+        if w.lower() == word:
+            count += 1
+    return count
 
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
 
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
-    else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+def sanitize(corpus):
+    pure_corpus = []
+    for word in corpus:
+        if count_occurrences(word, pure_corpus) == 0:
+            pure_corpus.append(word)
+    return pure_corpus
