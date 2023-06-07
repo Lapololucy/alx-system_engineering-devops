22d73145e5044912da7e15a4c3e04ec9880f692d
Lapololucy <lapololucy@gmail.com>
2023-06-07 09:28:25 +0000

    fixing errors

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,65 +1,54 @@
 #!/usr/bin/python3
-"""
-Module for requesting the Reddit API
-"""
+"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
-URL = 'https://www.reddit.com'
-headers = {"User-Agent": "Custom Agent"}
 
 
-def count_words(subreddit, word_list):
-    """Prints a count of given keywords"""
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     try:
-        uri = URL + '/r/{}/hot.json'.format(subreddit)
-        hot_titles = fetch_hot_posts(uri)
-        pure_word_list = sanitize(word_list)
-        result = dict(zip(pure_word_list, [0] * len(pure_word_list)))
-        for title in hot_titles:
-            for word in pure_word_list:
-                occurrences = count_occurrences(word, title.split())
-                if occurrences:
-                    result[word] += occurrences
-        result = dict(sorted(result.items()))
-        for key, value in result.items():
-            if value:
-                print("{}: {}".format(key.lower(), value))
-        return result
-    except Exception as e:
-        print(e)
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
 
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
-def fetch_hot_posts(uri, after=None, hot_list=[]):
-    """Recursive function for the job"""
-    parameters = {}
-    if after:
-        parameters['after'] = after
-    res = requests.get(uri,
-                       headers=headers,
-                       allow_redirects=False,
-                       params=parameters)
-    if res.status_code == 200:
-        children = res.json().get('data').get('children')
-        for child in children:
-            hot_list.append(child.get('data').get('title'))
-        next_ = res.json().get('data').get('after')
-        if next_ is not None:
-            fetch_hot_posts(uri, next_, hot_list)
-        return hot_list
-
-
-def count_occurrences(word, corpus):
-    """Counts occurrences of a word in a list of words"""
-    word = word.lower()
-    count = 0
-    for w in corpus:
-        if w.lower() == word:
-            count += 1
-    return count
-
-
-def sanitize(corpus):
-    pure_corpus = []
-    for word in corpus:
-        if count_occurrences(word, pure_corpus) == 0:
-            pure_corpus.append(word)
-    return pure_corpus
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
